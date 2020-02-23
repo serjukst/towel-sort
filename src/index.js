63f0c4eb3185a441a13f8897309d668dc9d1f0e2
Serhii Stepanchuk <51526907+serjukst@users.noreply.github.com>
2020-02-23 23:40:00 +0200

    fix: fix solution for towel-sort task

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ module.exports = function towelSort (matrix) {
   if (!matrix || matrix.length === 0) return [];
 
   for (let i = 0; i < matrix.length; i++) {
+
    if (i % 2) {
      
      matrix[i].sort(function (a, b) {
        return a + b;
      });
-
    }
  }
+
  return matrix.join(',').split(',');
 }
