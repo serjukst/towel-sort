f63333efc1126714ba23cdc338eee421d1d97119
Serhii Stepanchuk <serjukst@gmail.com>
2020-02-24 10:05:06 +0200

    refactor: detele unnecessary line code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
   if (!matrix || matrix.length === 0) return [];
 
   for (let i = 0; i < matrix.length; i++) {
 
-   if (i % 2) {
-     
-     matrix[i].sort(function (a, b) {
-       return a + b;
-     });
-   }
+   if (i % 2) { matrix[i].reverse();}
+
   }
 
-    let res = matrix.join(',').split(',');
-    
-    for (let j = 0; j < res.length; j++) {
-        res[j] = parseInt(res[j]);
-    }
-
-    return res;
+    return matrix.join(',').split(',');
 }
